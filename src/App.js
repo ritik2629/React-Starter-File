38be8483e6449e5f3b7cd0d64968de5a85472146
Ritik Raj <ritikraj2629@gmail.com>
2021-12-25 20:25:16 +0530

    React Starter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App =()=>{
+  const myStyle={
+    textAlign:"center",
+    color:"#00a8ff",
+  }
+  return(
+    <div>
+      <h1 style={myStyle}>𝕳𝖊𝖑𝖑𝖔 𝕽𝖎𝖙𝖎𝖐 𝕴𝖓 𝕿𝖍𝖊 𝖂𝖔𝖗𝖑𝖉 𝕺𝖋 𝕽𝖊𝖆𝖈𝖙</h1>
+    </div>
+  )
+}
+export default App;
